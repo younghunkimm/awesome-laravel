43a8fb1cf16bf3222685ed2374d38cd1f4864d34
김영훈 <hunifive@naver.com>
2023-12-15 21:29:37 +0900

    ~212p

---
+++ tests/Feature/Http/Controllers/Auth/SocialLoginControllerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use App\Enums\Provider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Socialite\Contracts\User as SocialiteUser;
+use Laravel\Socialite\Facades\Socialite;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+class SocialLoginControllerTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function testRedirectToProvider(): void
+    {
+        $provider = Provider::Github;
+
+        /** @see \Laravel\Socailite\Two\GithubProvider::getAuthUrl() */
+        $this->get(route('login.social', $provider))
+            ->assertRedirectContains('https://github.com/login/oauth/authorize');
+    }
+
+    public function testSocialLoginAndUpdateOrCreateUser(): void
+    {
+        $provider = Provider::Github;
+
+        $data = [
+            'email' => $this->faker->safeEmail(),
+            'name' => $this->faker->name(),
+        ];
+
+        $socialUser = $this->mock(SocialiteUser::class, function(MockInterface $mock) use ($data) {
+            $mock->shouldReceive('getEmail')
+                ->andReturn($data['email']);
+            $mock->shouldReceive('getName')
+                ->andReturn($data['name']);
+        });
+
+        Socialite::shouldReceive('driver->user')
+            ->once()
+            ->andReturn($socialUser);
+
+        $this->get(route('login.social.callback', $provider))
+            ->assertRedirect();
+
+        $this->assertEquals(
+            session()->socialite($provider),
+            $socialUser->getEmail()
+        );
+
+        $this->assertAuthenticated();
+
+        $this->assertDatabaseHas('users', $data);
+    }
+}
---
+++ tests/Feature/Providers/SessionServiceProviderTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\Providers;
+
+use App\Enums\Provider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Session;
+use Tests\TestCase;
+
+class SessionServiceProviderTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * A basic feature test example.
+     */
+    public function testSocialiteMacro(): void
+    {
+        $this->assertTrue(
+            Session::hasMacro('socialite')
+        );
+
+        Session::socialite(Provider::Github, $this->faker->safeEmail());
+
+        $this->assertTrue(
+            Session::has('socialite.github')
+        );
+    }
+
+    public function testSocialiteMissingAllMacro(): void
+    {
+        $this->assertTrue(
+            Session::hasMacro('socialiteMissingAll')
+        );
+
+        $this->assertTrue(
+            Session::socialiteMissingAll()
+        );
+
+        Session::put('socialite.github', $this->faker->safeEmail());
+
+        $this->assertFalse(
+            Session::socialiteMissingAll()
+        );
+    }
+}
